c647e7af11c30b6476aa97c50da55b93361c4071
Alex Smirnov <workpost@amurlife.ru>
2019-01-29 16:58:50 +0900

    Chanched color

--- wp-content/themes/bymi/sidebar.php
+++ wp-content/themes/bymi/sidebar.php
@@ -5,27 +5,27 @@
  * @package Full Page
  */
 ?>
-<div id="sidebar">    
+<div id="sidebar">
     <?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
         <h3 class="widget-title"><?php esc_html_e( 'Category', 'full-page' ); ?></h3>
-        <aside id="categories" class="widget">           
+        <aside id="categories" class="widget">
             <ul>
                 <?php wp_list_categories('title_li=');  ?>
             </ul>
         </aside>
        <h3 class="widget-title"><?php esc_html_e( 'Archives', 'full-page' ); ?></h3>
-        <aside id="archives" class="widget">           
+        <aside id="archives" class="widget">
             <ul>
                 <?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
             </ul>
         </aside>
          <h3 class="widget-title"><?php esc_html_e( 'Meta', 'full-page' ); ?></h3>
-         <aside id="meta" class="widget">           
+         <aside id="meta" class="widget">
             <ul>
                 <?php wp_register(); ?>
                 <li><?php wp_loginout(); ?></li>
                 <?php wp_meta(); ?>
             </ul>
         </aside>
-    <?php endif; // end sidebar widget area ?>	
+    <?php endif; // end sidebar widget area ?>
 </div><!-- sidebar -->
